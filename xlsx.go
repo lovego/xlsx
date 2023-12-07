72a4bd77a9238ecd7b208fad32a325b5d6620a8c
廖恒晨 <liaohengchen@gmail.com>
2023-12-07 15:54:24 +0800

    generate sheet

--- xlsx.go
+++ xlsx.go
@@ -46,8 +46,8 @@ func Write(w io.Writer, sheets ...Sheet) error {
 func Generate(sheets ...Sheet) (*xlsx.File, error) {
 	file := xlsx.NewFile()
 	for i := range sheets {
-		// no generate sheet when no columns or no data
-		if len(sheets[i].Columns) == 0 && sheets[i].Data == nil {
+		// no generate sheet when no columns
+		if len(sheets[i].Columns) == 0 {
 			continue
 		}
 		if err := sheets[i].Generate(file); err != nil {
